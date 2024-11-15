75b037edd3f4a607604a01f96d020d3777c8641a
mern-sejin <mern.sejin@gmail.com>
2024-11-15 07:06:20 -0800

    simple-python-problem-solving

---
+++ math-set.py
@@ -0,0 +1,9 @@
+# union
+a = { 1, 2, 3, 4, 5 }
+b = { 5, 2, 3, 8, 9 }
+print('union:', a.union(b))
+
+# intersection
+c = { 5, 3, 8, 9, 10 }
+d = { 2, 3, 5, 1, 9 }
+print('intersection:', c.intersection(d))
